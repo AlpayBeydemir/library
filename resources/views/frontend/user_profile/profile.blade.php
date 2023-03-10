797be8b25a55c004ee3734797c2db6cd7c64d3dc
AlpayBeydemir <alpay.beydemir@hotmail.com>
2023-03-10 13:06:19 +0300

    ds

--- resources/views/frontend/user_profile/profile.blade.php
+++ resources/views/frontend/user_profile/profile.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
-    {{ $profile->name }}
+
+    <div class="container mt-5">
+
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-lg-6">
+                <h4>My Information</h4>
+                <div class="mt-3">
+                    <label class="form-label" for="name">User Name</label>
+                    <input class="form-control" type="text" name="name" placeholder="{{ $profile->name }}">
+                </div>
+
+                <div class="mt-3">
+                    <label class="form-label" for="name">Email</label>
+                    <input class="form-control" type="email" name="email" placeholder="{{ $profile->email }}">
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
 
 @endsection()
